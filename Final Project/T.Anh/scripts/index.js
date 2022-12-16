23af77952b2dd7c00a3cc2aa2bf8197ed63dbc0a
unknown <huynhphamthuyanh.126@gmail.com>
2022-12-16 22:29:36 +0700

    update

---
+++ Final Project/T.Anh/scripts/index.js
@@ -0,0 +1,33 @@
+/**
+ * LOADING OVERLAY
+ */
+// var loadingOverlay = document.querySelector(".loading");
+// function toggleLoading(event) {
+//   if (event.keyCode !== 13) return;
+
+//   document.activeElement.blur();
+
+//   if (loadingOverlay.classList.contains("hidden")) {
+//     loadingOverlay.classList.remove("hidden");
+//   } else {
+//     loadingOverlay.classList.add("hidden");
+//   }
+// }
+// document.addEventListener("keydown", toggleLoading);
+
+// // Tắt overlay khi các tài nguyên của trang load xong
+// window.addEventListener("load", function () {
+//     document.getElementById("overlay").remove();
+// });
+
+// DROPDOWN APPEARANCE EFFECT
+jQuery(document).ready(function () {
+  $(".dropdown").hover(
+    function () {
+      $(".dropdown-menu", this).fadeIn(300);
+    },
+    function () {
+      $(".dropdown-menu", this).fadeOut(300);
+    }
+  );
+});
