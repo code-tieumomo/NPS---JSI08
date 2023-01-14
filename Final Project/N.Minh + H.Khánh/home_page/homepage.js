1640dcb669666d332dbeb7da5ab8edea17522ed7
unknown <falkunboy15@gmail.com>
2023-01-14 17:34:37 +0700

    new

--- Final Project/N.Minh + H.Khánh/home_page/homepage.js
+++ Final Project/N.Minh + H.Khánh/home_page/homepage.js
@@ -1,5 +1,5 @@
-// var counter = 1;
-// setInterval(function () {
-//     counter = counter + 1 <= 3 ? counter + 1 : 1;
-//     document.getElementById("radio" + counter).checked = true;
-// }, 5000);
+var counter = 1;
+setInterval(function () {
+    counter = counter + 1 <= 3 ? counter + 1 : 1;
+    document.getElementById("radio" + counter).checked = true;
+}, 5000);
